125492f7bdf1ba916dd0c1746bacf14d061e0fe3
rajrana22 <rrajdeeparanaj@gmail.com>
2016-05-05 11:37:53 +0800

    This is what I've got so far
    It only knows how much elements there needs to be, but the actual balance must change the coefficients only.

--- bep.py
+++ bep.py
@@ -2,41 +2,72 @@ import re
 
 equation = raw_input("Insert an equation:")
 
+# Let us first split left and right side of equations, i.e. split them over '='
+# sign.
+split_over_equal = equation.split('=')
+if len(split_over_equal) != 2 :
+    raise ValueError("You didn't provide left-right side of the equation")
 
-split_over_equal = equation.split("=")
+# variables to hold left and right side.
+leftSide = None
+rightSide = None
+for index, side in enumerate(split_over_equal):
+    # We split each compound in a dictionary object
+    compositionAsDict = {}
+    for compound in side.split('+'):
+        # If we print compounds, we may still have spaces within the strings.
+        # Those need not be removed here as regular expression below should
+        # take are of that.
+        # print("Compound: " + compound)
 
-for side in split_over_equal:
-
-    for compound in side.split("+"):
-        print("Compound: " + compound)
-        compositionAsDict = {}
+        # Regex doesn't support grouping into dictionary and searching repeatedly.
+        # So search for the string, then zero it till we reach the end of
+        # string.
         remaining = compound
 
-        moleculeCount = None
+        moleculeCount = 1
         while remaining:
-            elements = re.search(r"(?P<Molecules_Count>[0-9])?(?:\s*)(?P<Element>[A-Z][a-z]*)(_(?P<Atoms_Count>[0-9]+))?(?:\s*)",
+            elements = re.search(r'(?P<Molecules_Count>[0-9])?(?:\s*)(?P<Element>[A-Z][a-z]*)(_(?P<Atoms_Count>[0-9]+))?(?:\s*)',
                     remaining, re.M)
-            if not moleculeCount:
-                moleculeCount = 1 if not elements.group("Molecules_Count") else int(elements.group("Molecules_Count"))
+            if elements.group('Molecules_Count'):
+                moleculeCount = int(elements.group('Molecules_Count'))
 
-            atomsCount = 1 if not elements.group("Atoms_Count") else int(elements.group("Atoms_Count"))
-            compositionAsDict[elements.group("Element")] = moleculeCount * atomsCount
-
+            atomsCount = moleculeCount
+            if elements.group('Atoms_Count'):
+                atomsCount = int(elements.group('Atoms_Count'))
+            if elements.group('Element') in compositionAsDict:
+                compositionAsDict[elements.group('Element')] = compositionAsDict[elements.group('Element')]
+            else:
+                compositionAsDict[elements.group('Element')] = atomsCount
             remaining = remaining[elements.end():]
-        else:
-            print compositionAsDict
+    if index == 0:
+        leftSide = compositionAsDict
+    else:
+        rightSide = compositionAsDict
 
-left, right = split_over_equal
+print "Initial left side of equation:"
+print leftSide
+print "Initial right side of equation:"
+print rightSide
 
-if left == right:
+unmatchedItems = set(leftSide.keys()) ^ set(rightSide.keys())
+if len(unmatchedItems):
+    print("Following keys are only present on one-side of the equation")
+    print " ".join(str(x) for x in unmatchedItems)
+    raise ValueError("Can't balance equations due to unmatched elements")
 
-    print(equation)
+# Now by definition we should have same keys (elements) on each side
 
-elif right > left:
-        left += 1
+for key in leftSide:
+    if leftSide[key] < rightSide[key]:
+        print("A number of atoms are different on the left and right side for Element " + "'" + key + "'")
+        print("Balancing Element " + "'" + key + "'" + " on right side")
+        leftSide[key] += 1
+    elif leftSide[key] > rightSide[key]:
+        rightSide[key] += 1
 
-else:
-    while left > right:
-        right += 1
+print "Final left side equation:"
+print leftSide
 
-print left + "=" + right
+print "Final right side equation:"
+print rightSide
